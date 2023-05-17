2bd9b6473e8201c686a7c770fca7b53dc7777407
Lucas Irineu <lucasirineu119@gmail.com>
2023-05-17 15:54:21 -0300

    requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    egg = Ingredient('ovo')
+    egg_copy = Ingredient('ovo')
+    salmon = Ingredient('salmão')
+
+    assert egg.__hash__() == hash('ovo')
+    assert egg.__repr__() == "Ingredient('ovo')"
+    assert egg.name == 'ovo'
+    assert egg.restrictions == {Restriction.ANIMAL_DERIVED}
+
+    assert egg.__eq__(salmon) is False
+    assert egg.__eq__(egg_copy) is True
+
+    assert hash(salmon) != hash(egg)
